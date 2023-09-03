90e8d36decc241694f162341e310fd214ff5d857
nomuyoshi <nomuyoshi0111@gmail.com>
2023-09-03 17:10:49 +0900

    handler自作 Handleメソッド雰囲気実装(まだまだ処理が足りない)

--- sdk/myhandler.go
+++ sdk/myhandler.go
@@ -1,13 +1,17 @@
 package sdk
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"log/slog"
+	"time"
 )
 
 type (
 	MyHandler struct {
 		opts Options
+		w    io.Writer
 	}
 
 	Options struct {
@@ -31,4 +35,58 @@ func (h *MyHandler) Enabled(_ context.Context, l slog.Level) bool {
 	}
 
 	return l >= minLevel
+}
+
+// Handle methods that produce output should observe the following rules:
+//   - If r.Time is the zero time, ignore the time.
+//   - TODO: If r.PC is zero, ignore it.
+//   - TODO: Attr's values should be resolved.
+//   - TODO: If an Attr's key and value are both the zero value, ignore the Attr.
+//     This can be tested with attr.Equal(Attr{}).
+//   - TODO: If a group's key is empty, inline the group's Attrs.
+//   - TODO: If a group has no Attrs (even if it has a non-empty key),
+//     ignore it.
+func (h *MyHandler) Handle(ctx context.Context, r slog.Record) error {
+	output := newOutput(new(bytes.Buffer))
+	output.buf.WriteByte('{')
+
+	if !r.Time.IsZero() {
+		output.appendKey(slog.TimeKey)
+		output.appendTime(r.Time)
+	}
+	output.appendKey(slog.LevelKey)
+	output.appendString(r.Level.String())
+
+	output.appendKey(slog.MessageKey)
+	output.appendString(r.Message)
+	output.buf.WriteByte('}')
+	output.buf.WriteByte('\n')
+
+	_, err := h.w.Write(output.buf.Bytes())
+	return err
+}
+
+type output struct {
+	buf *bytes.Buffer
+}
+
+func newOutput(buf *bytes.Buffer) *output {
+	return &output{buf: buf}
+}
+
+func (o *output) appendKey(key string) {
+	o.appendString(key)
+	o.buf.WriteByte(':')
+}
+
+func (o *output) appendString(str string) {
+	o.buf.WriteByte('"')
+	o.buf.WriteString(str) // TODO strをescape
+	o.buf.WriteByte('"')
+}
+
+func (o *output) appendTime(t time.Time) {
+	o.buf.WriteByte('"')
+	o.buf.WriteString(t.Format(time.RFC3339Nano))
+	o.buf.WriteByte('"')
 }
