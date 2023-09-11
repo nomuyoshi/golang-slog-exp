25f5a21011ca44077ef6435a9c296a8dde97bf6c
nomuyoshi <nomuyoshi0111@gmail.com>
2023-09-11 10:49:43 +0900

    基本的な使い方を試す

--- README.md
+++ README.md
@@ -1,7 +1,57 @@
 # 概要
 golangに追加されたslogを試してみるリポジトリ
 
-# sdk
+# custom
 組み込みのJSONHandlerを参考に自作した Handler  
 色々考慮することが多いので実務でslogのHandlerを自作することはなさそう。  
 JSONHandlerで十分。  
+
+# main.go
+自作のHanlderや組み込みのJSONHandlerを使ったログ出力処理
+
+# slog感想
+Hanlderを自作するのはとっても大変。組み込みのHanlderで色々できるのでゼロから自作することはないはず。
+
+よく使いそうなカスタマイズ
+
+- `Logger.With` を使って traceIDやsessionIDのような共通項目を出力させることができる
+- `LogValuer` インターフェースを実装（`LogValue()` メソッド）してパスワードとか機密性の高い情報をマスクする
+- `ReplaceAttr` オプションで出力内容をなんかいじる
+
+# その他
+[Go 1.21連載始まります＆slogをどう使うべきか](https://future-architect.github.io/articles/20230731a/) が勉強になった
+
+何か凝ったカスタマイズが必要になったら、記事にあるようにJSONHandlerのラッパーを作ると良いかもしれない。
+
+以下 [Go 1.21連載始まります＆slogをどう使うべきか](https://future-architect.github.io/articles/20230731a/) より引用
+
+```
+// ハンドラーのラッパー
+type WriteTraceIDHandler struct {
+	parent slog.Handler
+}
+
+func WithWriteTraceIDHandler(parent slog.Handler) *WriteTraceIDHandler {
+	return &WriteTraceIDHandler{
+		parent: parent,
+	}
+}
+
+// ログ出力に情報を付与するメソッド
+func (h *WriteTraceIDHandler) Handle(ctx context.Context, record slog.Record) error {
+	record.Add(slog.String("traceID", ctx.Value(ctxKey).(string)))
+	return h.parent.Handle(ctx, record)
+}
+
+func (h *WriteTraceIDHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.parent.Enabled(ctx, level)
+}
+
+func (h *WriteTraceIDHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &WriteTraceIDHandler{h.parent.WithAttrs(attrs)}
+}
+
+func (h *WriteTraceIDHandler) WithGroup(name string) slog.Handler {
+	return &WriteTraceIDHandler{h.parent.WithGroup(name)}
+}
+```
--- main.go
+++ main.go
@@ -1,22 +1,101 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
-	"slogexp/sdk"
 
 	"log/slog"
+	"slogexp/custom"
+
+	"github.com/google/uuid"
 )
 
 func main() {
-	logger := slog.New(sdk.NewMyHandler(os.Stdout, nil))
-	logger = logger.With("trace_id", "adkjk290jwe3w03233=")
-	logger.Info("slog体験", "count", 4)
+	u := NewUser(1, "testpass", "exampleemail@test.com")
+	fmt.Println("========= 自作Handler =========")
+	// 自作 Handlerお試し
+	clogger := slog.New(custom.NewMyHandler(os.Stdout, nil))
+	clogger = clogger.With("trace_id", "adkjk290jwe3w03233=")
+	clogger.Info("slog体験", "count", 4)
 	// => {"time":"2023-09-09T23:39:33.922167+09:00","level":"INFO","msg":"slog体験","trace_id":"adkjk290jwe3w03233=","count":4}
-	userLogger := logger.WithGroup("user")
-	userLogger = userLogger.With("service", "sns")
-	userLogger.Info("コメント投稿完了", "user_id", 1, "comment_id", 1000)
+	clogger = clogger.WithGroup("user")
+	clogger = clogger.With("service", "sns")
+	clogger.Info("コメント投稿完了", "user_id", 1, "comment_id", 1000, "user", u)
 	// => {"time":"2023-09-09T23:39:33.922331+09:00","level":"INFO","msg":"コメント投稿完了","trace_id":"adkjk290jwe3w03233=","user":{"service":"sns","user_id":1,"comment_id":1000}}
-	userLogger.Error("コメント投稿エラー", "user_id", 1, "comment", "ああああ", "error", fmt.Errorf("hogehoge").Error())
+	clogger.Error("コメント投稿エラー", "user_id", 1, "comment", "ああああ", "error", fmt.Errorf("hogehoge").Error())
 	// => {"time":"2023-09-09T23:42:00.869847+09:00","level":"ERROR","msg":"コメント投稿エラー","trace_id":"adkjk290jwe3w03233=","user":{"service":"sns","user_id":1,"comment":"ああああ","error":"hogehoge"}}
+
+	// 組み込みのJSONHandlerカスタマイズ
+	fmt.Println("========= 組み込みJSONHandler =========")
+	opts := slog.HandlerOptions{
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				a.Key = "時間"
+				return a
+			}
+			if a.Value.Kind() == slog.KindString {
+				// いろいろできる
+			}
+
+			return a
+		},
+	}
+	dlogger := slog.New(slog.NewJSONHandler(os.Stdout, &opts))
+	// Userをまるごとログに渡したときもPassword, Emailを伏せ字にしたいなら、UserもLogValuerを実装している必要がある
+	dlogger.Info("ユーザー登録完了", "user", u)
+	dlogger.Info("ユーザー登録完了", "pass", u.Password, "email", u.Email)
+	// trace_idをつけてみる
+	ctx := withTraceID(context.Background())
+	dlogger = withTraceIDLogger(ctx, dlogger)
+	dlogger.Info("/hogehoge API request")
+	dlogger.Info("/hogehoge API response")
+}
+
+type _CtxKeyTypeTraceID struct{}
+
+var ctxTraceIDKey = _CtxKeyTypeTraceID{}
+
+func withTraceID(ctx context.Context) context.Context {
+	traceID, _ := uuid.NewUUID()
+	return context.WithValue(ctx, ctxTraceIDKey, traceID.String())
+}
+
+func withTraceIDLogger(ctx context.Context, l *slog.Logger) *slog.Logger {
+	v := ctx.Value(ctxTraceIDKey).(string)
+	return l.With("traceID", v)
+}
+
+type (
+	User struct {
+		ID       int
+		Password Password
+		Email    Email
+	}
+	Password string
+	Email    string
+)
+
+func NewUser(id int, pass Password, email Email) *User {
+	return &User{
+		ID:       id,
+		Password: pass,
+		Email:    email,
+	}
+}
+
+func (p Password) LogValue() slog.Value {
+	return slog.StringValue("**********")
+}
+
+func (e Email) LogValue() slog.Value {
+	return slog.StringValue("*****@****")
+}
+
+func (u *User) LogValue() slog.Value {
+	return slog.AnyValue(User{
+		ID:       u.ID,
+		Password: "**********",
+		Email:    "*****@****",
+	})
 }
